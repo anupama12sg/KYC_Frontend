fa609ec3bdc3a6d57d40b5eee2915682f4b71d00
Anupama Singh <anupama12sg@gmail.com>
2023-10-02 22:40:04 +0530

    Changed single to double quotes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,15 +14,15 @@ import{
 } from "react"
 
 export default function Home() {
- const [input, setInput]= useState('')
+ const [input, setInput]= useState("")
 const handleInputChange= (e: any)=> setInput(e.target.value)
-const isError= input === ''
+const isError= input === ""
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <FormControl isInvalid={isError}>
       <FormLabel>Email</FormLabel>
-      <Input type='email' value={input} onChange={handleInputChange} />
+      <Input type="email" value={input} onChange={handleInputChange} />
       {!isError ? (
         <FormHelperText>
           Enter the email you'd like to receive the newsletter on.
